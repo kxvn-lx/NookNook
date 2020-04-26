62ec7ac63685bb2bd6c51e8d655ffd79a54b0ad1
Kevin Laminto <kevin.laminto@gmail.com>
2020-04-26 12:47:44 +1000

    Fixed active critters this month algorithm issue

--- NookNook/App/Dashboard/CritterHelper.swift
+++ NookNook/App/Dashboard/CritterHelper.swift
@@ -12,15 +12,15 @@ struct CritterHelper {
     
     /**
      Method to calculate every critter that is available this month.
-    - Parameters:
-        - userHemisphere: The user's current hemisphere
-    - Returns:
-        Array Bugs and Fishes in the current hemisphere on this month.
+     - Parameters:
+     - userHemisphere: The user's current hemisphere
+     - Returns:
+     Array Bugs and Fishes in the current hemisphere on this month.
      */
     static func parseCritter(userHemisphere: DateHelper.Hemisphere) -> ([Critter], [Critter]) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "M"
-        let currMonthInt = Int(dateFormatter.string(from: Date()))! - 1
+        let currMonthInt = Int(dateFormatter.string(from: Date()))!
         
         
         var northernBugs: [Critter] = []
@@ -35,32 +35,43 @@ struct CritterHelper {
         switch userHemisphere {
         case .Northern:
             allBugs.forEach({
-                if TimeMonthEngine.formatMonths(month: $0.activeMonthsN).contains(currMonthInt) || TimeMonthEngine.formatMonths(month: $0.activeMonthsN) == [-1] {
+                let lowerBounds = TimeMonthEngine.formatMonths(month: $0.activeMonthsN) != [-1] ? TimeMonthEngine.formatMonths(month: $0.activeMonthsN).sorted(by: { $0 < $1 })[0] : -1
+                let upperBounds = TimeMonthEngine.formatMonths(month: $0.activeMonthsN) != [-1] ? TimeMonthEngine.formatMonths(month: $0.activeMonthsN).sorted(by: { $0 < $1 }).last! : -1
+                
+                if (lowerBounds...upperBounds).contains(currMonthInt) || TimeMonthEngine.formatMonths(month: $0.activeMonthsN) == [-1] {
                     // bug is in season for northern region.
                     northernBugs.append($0)
                 }
             })
             allFishes.forEach({
-                if TimeMonthEngine.formatMonths(month: $0.activeMonthsN).contains(currMonthInt) || TimeMonthEngine.formatMonths(month: $0.activeMonthsN) == [-1] {
+                let lowerBounds = TimeMonthEngine.formatMonths(month: $0.activeMonthsN) != [-1] ? TimeMonthEngine.formatMonths(month: $0.activeMonthsN).sorted(by: { $0 < $1 })[0] : -1
+                let upperBounds = TimeMonthEngine.formatMonths(month: $0.activeMonthsN) != [-1] ? TimeMonthEngine.formatMonths(month: $0.activeMonthsN).sorted(by: { $0 < $1 }).last! : -1
+                
+                if (lowerBounds...upperBounds).contains(currMonthInt) || TimeMonthEngine.formatMonths(month: $0.activeMonthsN) == [-1] {
                     // fish is in season for northern region.
                     northernFishes.append($0)
                 }
             })
         case .Southern:
             allBugs.forEach({
-                if TimeMonthEngine.formatMonths(month: $0.activeMonthsS).contains(currMonthInt) || TimeMonthEngine.formatMonths(month: $0.activeMonthsS) == [-1] {
+                let lowerBounds = TimeMonthEngine.formatMonths(month: $0.activeMonthsN) != [-1] ? TimeMonthEngine.formatMonths(month: $0.activeMonthsN).sorted(by: { $0 < $1 })[0] : -1
+                let upperBounds = TimeMonthEngine.formatMonths(month: $0.activeMonthsN) != [-1] ? TimeMonthEngine.formatMonths(month: $0.activeMonthsN).sorted(by: { $0 < $1 }).last! : -1
+                
+                if (lowerBounds...upperBounds).contains(currMonthInt) || TimeMonthEngine.formatMonths(month: $0.activeMonthsS) == [-1] {
                     // bug is in season for northern region.
                     southernBugs.append($0)
                 }
             })
             allFishes.forEach({
-                if TimeMonthEngine.formatMonths(month: $0.activeMonthsS).contains(currMonthInt) || TimeMonthEngine.formatMonths(month: $0.activeMonthsS) == [-1] {
+                let lowerBounds = TimeMonthEngine.formatMonths(month: $0.activeMonthsN) != [-1] ? TimeMonthEngine.formatMonths(month: $0.activeMonthsN).sorted(by: { $0 < $1 })[0] : -1
+                let upperBounds = TimeMonthEngine.formatMonths(month: $0.activeMonthsN) != [-1] ? TimeMonthEngine.formatMonths(month: $0.activeMonthsN).sorted(by: { $0 < $1 }).last! : -1
+                
+                if (lowerBounds...upperBounds).contains(currMonthInt) || TimeMonthEngine.formatMonths(month: $0.activeMonthsS) == [-1] {
                     // fish is in season for northern region.
                     southernFishes.append($0)
                 }
             })
         }
-        
         
         switch userHemisphere {
         case .Northern:
@@ -74,10 +85,10 @@ struct CritterHelper {
     /**
      Method to calculate caught critters ONLY on this month
      - Parameters:
-        - caughtBugs: The user's overall caught bugs
-        - caughtFishes: The user's overall caught fishes
-        - monthFishes: The current month's available fishes
-        - monthBugs: The current month's available bugs
+     - caughtBugs: The user's overall caught bugs
+     - caughtFishes: The user's overall caught fishes
+     - monthFishes: The current month's available fishes
+     - monthBugs: The current month's available bugs
      */
     static func parseCaughtCritter(caughtBugs: [Critter], caughtFishes: [Critter], monthBugs: [Critter], monthFishes: [Critter]) -> ([Critter], [Critter]) {
         
