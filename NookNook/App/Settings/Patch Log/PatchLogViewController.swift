86e7a45cc2587f20a25e2f989e557d26a84402f6
Kevin Laminto <kevin.laminto@gmail.com>
2020-05-08 12:01:48 +1000

    build 2020050615

--- NookNook/App/Settings/Patch Log/PatchLogViewController.swift
+++ NookNook/App/Settings/Patch Log/PatchLogViewController.swift
@@ -20,7 +20,7 @@ class PatchLogViewController: UIViewController {
     
     var logArray = [
         """
-        v1.0.0 (2020050614)
+        v1.0.0 (2020050615)
         - Added ads so your boy can pay rent lol (can be removed via in app purchases under settings)
         - Added swipe tableview cells hint for new user to let them know SWIPE RIGHT is an option (only first time)
         - Improved app architecture
